41d0c86605aa41bb6d1e46ed066a85bf802d732d
AliMoradpour <mr.mrdp@yahoo.com>
2023-05-06 06:56:21 +0430

    make the structures

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+const products = [
+  {
+    id: 1,
+    title: "React.js",
+    category: "frontend",
+    createdAt: "2023-5-31T15:02:00.411Z",
+  },
+  {
+    id: 2,
+    title: "Node.js",
+    category: "backend",
+    createdAt: "2023-5-31T15:02:00.556Z",
+  },
+  {
+    id: 3,
+    title: "Vue.js",
+    category: "frontend",
+    createdAt: "2023-5-01T10:02:00.889Z",
+  },
+];
+
+const categories = [
+  {
+    id: 1,
+    title: "frontend",
+    description: "frontend of applications",
+    createdAt: "2023-5-31T15:02:00.411Z",
+  },
+  {
+    id: 2,
+    title: "backend",
+    description: "backend of applications",
+    createdAt: "2023-5-31T15:02:00.556Z",
+  },
+];
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-600">hello bitch</h1>
+    </>
+  );
+}
+
+export default App;
