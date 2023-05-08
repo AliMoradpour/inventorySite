427ec5d49da2238377032ea840b04efcdf9ce9be
AliMoradpour <mr.mrdp@yahoo.com>
2023-05-08 09:39:50 +0430

    add filter for categories

--- src/App.jsx
+++ src/App.jsx
@@ -10,40 +10,20 @@ function App() {
   const [productsList, setProductsList] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [sort, setSort] = useState("latest");
-  const [searchValue, setSearchValue] = useState();
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [searchValue, setSearchValue] = useState("");
 
   useEffect(() => {
     let result = productsList;
     result = filterSearchTitle(result);
+    result = filterSelectedCategory(result);
     result = sortDate(result);
     setFilteredProducts(result);
-  }, [productsList, sort, searchValue]);
-
-  const searchHandler = ({ target }) => {
-    setSearchValue(target.value.trim().toLowerCase());
-  };
-
-  const filterSearchTitle = (array) => {
-    return array.filter((p) => p.title.toLowerCase().includes(searchValue));
-  };
-
-  const sortHandler = ({ target }) => {
-    setSort(target.value);
-  };
-
-  const sortDate = (array) => {
-    let sortedProducts = [...array];
-    return sortedProducts.sort((a, b) => {
-      if (sort === "latest") {
-        return new Date(a.createdAt) > new Date(b.createdAt) ? -1 : 1;
-      } else if (sort === "earliest") {
-        return new Date(a.createdAt) > new Date(b.createdAt) ? 1 : -1;
-      }
-    });
-  };
+  }, [productsList, sort, searchValue, selectedCategory]);
 
   useEffect(() => {
-    const savedProducts = JSON.parse(localStorage.getItem("productsList")) || [];
+    const savedProducts =
+      JSON.parse(localStorage.getItem("productsList")) || [];
     const savedCategories =
       JSON.parse(localStorage.getItem("categoryList")) || [];
     setProductsList(savedProducts);
@@ -62,9 +42,41 @@ function App() {
     }
   }, [categoryList]);
 
+  const searchHandler = ({ target }) => {
+    setSearchValue(target.value.trim().toLowerCase());
+  };
+
+  const filterSearchTitle = (array) => {
+    return array.filter((p) => p.title.toLowerCase().includes(searchValue));
+  };
+
+  const sortHandler = ({ target }) => {
+    setSort(target.value);
+  };
+
+  const selectedCategoryHandler = ({ target }) => {
+    setSelectedCategory(target.value);
+  };
+
+  const sortDate = (array) => {
+    let sortedProducts = [...array];
+    return sortedProducts.sort((a, b) => {
+      if (sort === "latest") {
+        return new Date(a.createdAt) > new Date(b.createdAt) ? -1 : 1;
+      } else if (sort === "earliest") {
+        return new Date(a.createdAt) > new Date(b.createdAt) ? 1 : -1;
+      }
+    });
+  };
+
+  const filterSelectedCategory = (array) => {
+    if (!selectedCategory) return array;
+    return array.filter((item) => item.categoryId === selectedCategory);
+  };
+
   return (
     <div className="bg-slate-800 min-h-screen">
-      <Navbar />
+      <Navbar productsList={productsList} />
       <div className="container max-w-screen-sm mx-auto p-4">
         <Category setCategoryList={setCategoryList} />
         <Products
@@ -76,6 +88,9 @@ function App() {
           onSort={sortHandler}
           sort={sort}
           searchValue={searchValue}
+          categoryList={categoryList}
+          selectedCategory={selectedCategory}
+          onSelectedCategory={selectedCategoryHandler}
         />
         <ProductList
           productsList={filteredProducts}
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,4 +1,12 @@
-const Filter = ({onSort , onSearch , sort , searchValue}) => {
+const Filter = ({
+  onSort,
+  onSearch,
+  sort,
+  searchValue,
+  categoryList,
+  selectedCategory,
+  onSelectedCategory,
+}) => {
   return (
     <>
       <div className="flex items-center justify-between mb-6">
@@ -13,6 +21,7 @@ const Filter = ({onSort , onSearch , sort , searchValue}) => {
           onChange={onSearch}
         />
       </div>
+
       <div className="flex items-center justify-between mb-6">
         <label htmlFor="sort-product" className="text-slate-500 text-lg">
           Sort
@@ -31,6 +40,29 @@ const Filter = ({onSort , onSearch , sort , searchValue}) => {
           <option value="earliest" className="text-slate-300 bg-slate-500 ">
             earliest
           </option>
+        </select>
+      </div>
+
+      <div className="flex items-center justify-between mb-6">
+        <label htmlFor="sort-product" className="text-slate-500 text-lg">
+          Category
+        </label>
+        <select
+          name="sort-product"
+          className="bg-transparent text-slate-400 rounded-xl "
+          value={selectedCategory}
+          onChange={onSelectedCategory}>
+          <option value="" className="text-slate-300 bg-slate-500">
+            All
+          </option>
+          {categoryList.map((category) => (
+            <option
+              className="text-slate-300 bg-slate-500"
+              value={category.id}
+              key={category.id}>
+              {category.title}
+            </option>
+          ))}
         </select>
       </div>
     </>
